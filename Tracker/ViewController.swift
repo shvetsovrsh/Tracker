5cee836d69091d5cae0c6a68f645d40ffe36a6d7
shvetsovrsh <shvetsovrsh@icloud.com>
2023-09-01 19:10:51 +0500

    first commit

---
+++ Tracker/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Tracker
+//
+//  Created by Ruslan S. Shvetsov on 01.09.2023.
+//
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    }
+
+
+
+}
